b92389c321919d7913151375d7294e87067f41d3
xlc-soda <592789211@qq.com>
2018-06-13 09:47:23 +0800

    modification

--- 1/lab1_5.cpp
+++ 1/lab1_5.cpp
@@ -11,6 +11,7 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::ofstream;
+using std::ifstream;
 using std::ios;
 using std::getline;
 
@@ -22,20 +23,33 @@ int main(int argc,char * argv[]){
         cout<<"No arguments"<<endl;
         return 0;
     }
-    ofstream out(argv[1]);
-    if(!out.is_open()){
-        return 0;
-    }
-    cout<<"Input"<<endl;
-    while(true){
-	gets(s);
-	if(s==NULL||s[0]=='\n')
-		break;
-        int len=strlen(s);
-        if(len<1){
-            cout<<"break"<<endl;
-            return 0;
+    cout<<"Input 1 to input, 0 to output"<<endl;
+    int n;cin>>n;
+    if(n==1) {
+        ofstream out(argv[1]);
+        if(out.is_open()){
+            cout << "Input" << endl;
+            while (true) {
+                gets(s);
+                if (s == NULL || s[0] == '\n') {
+                    break;
+                }
+                int len = strlen(s);
+                if (len < 1) {
+                    cout << "break" << endl;
+                    return 0;
+                }
+                out << s << endl;
+            }
         }
-        out<<s<<endl;
+    }else{
+        ifstream out(argv[1]);
+        if(out.is_open()){
+            int cnt=1;
+            while(!out.eof()){
+                out.getline(s,1024);
+                cout<<cnt++<<' '<<s<<endl;
+            }
+        }
     }
 }
